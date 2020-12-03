650be03c7075771a65ee9895ead400a5d1b1ca8a
kaznak <kaznak.at.work@gmail.com>
2020-12-03 21:59:38 +0900

    App.tsx: Picking an image

--- App.tsx
+++ App.tsx
@@ -1,9 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import logo from "./assets/logo.png";
+import * as ImagePicker from "expo-image-picker";
+
+interface selectedImage {
+  localUri: string;
+}
 
 export default function App() {
+  const [
+    selectedImage,
+    setSelectedImage,
+  ] = React.useState<null | selectedImage>(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -14,10 +51,7 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
-      <TouchableOpacity
-        onPress={() => alert("Hello, world!")}
-        style={styles.button}
-      >
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
@@ -50,5 +84,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: "#fff",
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
   },
 });
