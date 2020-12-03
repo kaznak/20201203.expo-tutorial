0b8114964ac2741485a2c127d2895ba69645799d
kaznak <kaznak.at.work@gmail.com>
2020-12-03 21:45:53 +0900

    App.tsx: Creating a button

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import logo from "./assets/logo.png";
 
 export default function App() {
@@ -14,6 +14,12 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
+      <TouchableOpacity
+        onPress={() => alert("Hello, world!")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -35,5 +41,14 @@ const styles = StyleSheet.create({
     color: "#888",
     fontSize: 18,
     marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
